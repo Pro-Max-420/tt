36fc35f5839c5edbf68659207d39795ee4b4ff94
Pro-Max-420 <121747358+Pro-Max-420@users.noreply.github.com>
2023-02-04 23:18:24 +0600

    Update Tutul.py

--- Tutul.py
+++ Tutul.py
@@ -6,6 +6,10 @@ def xoss(z):
         time.sleep(0.050)
 xoss('\033[0;91m Checking Your Mobile...?');time.sleep(0.50)
 time.sleep(10)
+xoss("\033[0;92m Congratulations Your Device Support This Tools")
+xoss('\033[0;93m JOIN MY SCRIPT GIFT GROUP')
+time.sleep(3)
+os.system('xdg-open https://facebook.com/groups/554714119911648/')
 def Update():
     exit('\033[1;31m Commands On Update Please Wait For Update')
 def Run():
